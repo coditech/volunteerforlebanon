109a8485efe4122ebdc93ff1e866284593db222f
Xananax <xananax@yelostudio.com>
2018-05-21 01:32:54 +0300

    externalized utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,10 @@
+import { 
+  read_image_from_file as readImageFromFile,
+  to_slug as slugify,
+  serialize_form as serializeForm,
+  get_season_from_month as seasonFromMonth
+} from 'convenable'
 export * from './isEditMode'
 export * from './renderMarkdown'
-export * from './readImageFromFile'
-export * from './seasonFromMonth'
-export * from './serializeForm'
-export * from './slugify'
 export { toast } from 'react-toastify'
+export { readImageFromFile, slugify, seasonFromMonth, serializeForm }
