7ef39395d89c9174c0d4b39fd6d71a28a02ae612
Xananax <xananax@yelostudio.com>
2018-05-21 07:47:20 +0300

    better exports

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,10 @@
-import { 
+export { 
   read_image_from_file as readImageFromFile,
   to_slug as slugify,
   serialize_form as serializeForm,
-  get_season_from_month as seasonFromMonth
+  get_season_from_month as seasonFromMonth,
+  handle_form_submit as onSubmit
 } from 'convenable'
 export * from './isEditMode'
 export * from './renderMarkdown'
 export { toast } from 'react-toastify'
-export { readImageFromFile, slugify, seasonFromMonth, serializeForm }
